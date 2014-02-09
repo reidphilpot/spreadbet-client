5406e9b2b2a6bade35d556adae1f78b41d4e1ddb
Reid Philpot <reidphilpot@gmail.com>
2014-02-09 22:33:17 +0000

    removing unused node modules

--- web.js
+++ web.js
@@ -1,5 +1,4 @@
-var WebSocketServer = require('ws').Server
-    , http = require('http')
+var http = require('http')
     , express = require('express')
     , app = express()
     , port = process.env.PORT || 5000;
@@ -11,22 +10,6 @@ app.configure(function () {
     );
 });
 
-var server = http.createServer(app);
-server.listen(port);
-
-console.log('http server listening on %d', port);
-
-var wss = new WebSocketServer({server: server});
-console.log('websocket server created');
-wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
-
-    console.log('websocket connection open');
-
-    ws.on('close', function() {
-        console.log('websocket connection close');
-        clearInterval(id);
-    });
+var server = http.createServer(app).listen(port, function() {
+    console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
